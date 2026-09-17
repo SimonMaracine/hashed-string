b26cb40643ec8d60485c724a0c244f1d0aa8b93c
SimonMaracine <simonmara.dev@gmail.com>
2026-09-17 20:42:19 +0300

    Tweak code and change license

--- README.md
+++ README.md
@@ -8,22 +8,22 @@ Simply copy the header file and integrate it into your build system however you 
 #include <print>
 #include <unordered_map>
 
-#include "hash.hpp"
+#include "hashed_string.hpp"
 
-using namespace hash::literals;
+using namespace hashed_string::literals;
 
 int main() {
-    static constexpr auto h1 = hash::HashedStr32();
-    static constexpr auto h2 = hash::HashedStr32("some_string");
+    static constexpr auto h1 = hashed_string::HashedStr32();
+    static constexpr auto h2 = hashed_string::HashedStr32("some_string");
     static constexpr auto h3 = "some_other_string"_h;
-    const auto h4 = hash::HashedStr32(std::string("dynamic_string"));
+    const auto h4 = hashed_string::HashedStr32(std::string("dynamic_string"));
 
     std::println("{}", h1);
     std::println("{}", h2);
     std::println("{}", h3);
     std::println("{}", h4);
 
-    std::unordered_map<hash::HashedStr64, int, hash::StrHash<hash::HashedStr64>> map;
+    std::unordered_map<hashed_string::HashedStr64, int, hashed_string::StrHash<hashed_string::HashedStr64>> map;
     map["one"_H] = 1;
     map["two"_H] = 2;
     map["three"_H] = 3;
--- hashed_string.hpp
+++ hashed_string.hpp
@@ -6,8 +6,8 @@
 
 // Fowler-Noll-Vo algorithm, FNV-1a more exactly
 
-namespace hash {
-    namespace internal {
+namespace hashed_string {
+    namespace detail {
         struct Variant32 {
             using Type = std::uint32_t;
             static constexpr std::uint32_t FNV_OFFSET_BASIS = 2166136261u;
@@ -25,19 +25,19 @@ namespace hash {
         public:
             using Type = T::Type;
 
-            constexpr HashedStr() noexcept
+            constexpr HashedStr()
                 : hash(0) {}
 
-            explicit consteval HashedStr(const char* const string) noexcept
+            explicit consteval HashedStr(const char* const string)
                 : hash(fnv1a(string)) {}
 
-            explicit HashedStr(const std::string& string) noexcept
+            explicit HashedStr(const std::string& string)
                 : hash(fnv1a(string.c_str())) {}
 
-            constexpr operator Type() const noexcept { return hash; }
-            constexpr bool operator==(const HashedStr other) const noexcept { return hash == other.hash; }
+            constexpr operator Type() const { return hash; }
+            constexpr bool operator==(const HashedStr& other) const { return hash == other.hash; }
         private:
-            static constexpr Type fnv1a(const char* const string) noexcept {
+            static constexpr Type fnv1a(const char* const string) {
                 Type hash = T::FNV_OFFSET_BASIS;
 
                 for (std::size_t i {}; string[i] != '\0'; i++) {
@@ -52,23 +52,23 @@ namespace hash {
         };
     }
 
-    using HashedStr32 = internal::HashedStr<internal::Variant32>;
-    using HashedStr64 = internal::HashedStr<internal::Variant64>;
+    using HashedStr32 = detail::HashedStr<detail::Variant32>;
+    using HashedStr64 = detail::HashedStr<detail::Variant64>;
 
     namespace literals {
-        consteval HashedStr32 operator""_h(const char* const string, std::size_t) noexcept {
+        consteval HashedStr32 operator""_h(const char* const string, std::size_t) {
             return HashedStr32(string);
         }
 
-        consteval HashedStr64 operator""_H(const char* const string, std::size_t) noexcept {
+        consteval HashedStr64 operator""_H(const char* const string, std::size_t) {
             return HashedStr64(string);
         }
     }
 
     template<typename T>
     struct StrHash {
-        constexpr std::size_t operator()(const T hashed_string) const noexcept {
-            // Standard library's hash function is already perfect
+        constexpr std::size_t operator()(const T hashed_string) const {
+            // Standard library's hash function is already fine
             return std::hash<typename T::Type>()(hashed_string);
         }
     };
